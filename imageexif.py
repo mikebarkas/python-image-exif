d26c30c91951e476df92c40155bb82f8a368362e
Mike Barkas <mike@barkas.com>
2016-10-13 16:55:56 -0400

    Add convert function for gps degrees.

--- imageexif.py
+++ imageexif.py
@@ -31,6 +31,23 @@ def camera_data(data):
     return {key:value for key,value in decoded.items() if key in camera_tags()}
 
 
+def convert_to_degress(value):
+    """Change gps exif data into degrees."""
+    d0 = value[0][0]
+    d1 = value[0][1]
+    d = float(d0) / float(d1)
+
+    m0 = value[1][0]
+    m1 = value[1][1]
+    m = float(m0) / float(m1)
+
+    s0 = value[2][0]
+    s1 = value[2][1]
+    s = float(s0) / float(s1)
+
+    return d + (m / 60.0) + (s / 3600.0)
+
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('image', help='Image file name')
